e40f359a0a293c81c82b84dbe0ed541f54cdb517
Justin Phlegar <Justin.Phlegar@orasi.com>
2015-06-12 11:10:27 -0400

    1.0.4 - Added two new constants, Element_Timeout and Page_Timeout

--- src/main/java/com/orasi/utils/Constants.java
+++ src/main/java/com/orasi/utils/Constants.java
@@ -41,6 +41,12 @@ public class Constants {
     /** The default timeout in seconds, should be a generous default time */
     final static public int DEFAULT_GLOBAL_DRIVER_TIMEOUT = 60;
 
+    /** The timeout (seconds) for finding web elements on a page, shouldn't be too long */
+    final static public int ELEMENT_TIMEOUT = 10;
+    
+    /** The timeout (seconds) for page/DOM/transitions, should also be a generous */
+    final static public int PAGE_TIMEOUT = 60;
+    
     /** System properties */
 
     public static final String APPLICATION_UNDER_TEST = "selenium.applicationUnderTest";
