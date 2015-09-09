626040b3cb20fe0946f119a8180ef75cd01c689b
Justin Phlegar <Justin.Phlegar@orasi.com>
2015-09-08 20:55:49 -0400

    Unit Test - AlertHandler and FrameHandler

---
+++ src/test/java/com/orasi/utils/TestFrameHandler.java
@@ -0,0 +1,49 @@
+package com.orasi.utils;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.orasi.core.interfaces.Element;
+import com.orasi.core.interfaces.impl.ElementImpl;
+
+public class TestFrameHandler extends TestEnvironment{
+    @BeforeTest(groups ={"regression", "utils", "dev"})
+    @Parameters({ "runLocation", "browserUnderTest", "browserVersion",
+	    "operatingSystem", "environment" })
+    public void setup(@Optional String runLocation, String browserUnderTest,
+	    String browserVersion, String operatingSystem, String environment) {
+	setApplicationUnderTest("Test Site");
+	setBrowserUnderTest(browserUnderTest);
+	setBrowserVersion(browserVersion);
+	setOperatingSystem(operatingSystem);
+	setRunLocation(runLocation);
+	setTestEnvironment(environment);
+	setDefaultTestTimeout(3);
+	setPageURL("http://orasi.github.io/Selenium-Java-Core/sites/unitTests/orasi/utils/frameHandler.html");
+	testStart("TestFrame");
+    }
+    
+    @AfterTest(groups ={"regression", "utils", "dev"})
+    public void close(){
+	endTest("TestFrame");
+    }
+    
+    @Test(groups ={"regression", "utils", "dev"})
+    public void findAndSwitchToFrameFromOutsideFrame(){
+	FrameHandler.findAndSwitchToFrame(this, "menu_page" );
+	Assert.assertTrue(getDriver().findElement(By.id("googleLink")).isDisplayed());
+    }
+    
+    @Test(groups ={"regression", "utils", "dev"}, dependsOnMethods="findAndSwitchToFrameFromOutsideFrame")
+    public void findAndSwitchToFrameFromInsideDiffFrame(){
+	FrameHandler.findAndSwitchToFrame(this, "main_page" );
+	Assert.assertTrue(getDriver().findElement(By.id("button")).isDisplayed());
+    }
+    
+   
+}
