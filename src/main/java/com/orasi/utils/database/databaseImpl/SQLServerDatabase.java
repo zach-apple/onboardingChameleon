ed5459db0dc9f564f938ead6577551f6570d0f4e
Justin Phlegar <Justin.Phlegar@orasi.com>
2015-10-23 16:32:49 -0400

    Removed unused input parameter

--- src/main/java/com/orasi/utils/database/databaseImpl/SQLServerDatabase.java
+++ src/main/java/com/orasi/utils/database/databaseImpl/SQLServerDatabase.java
@@ -3,7 +3,7 @@ package com.orasi.utils.database.databaseImpl;
 import com.orasi.utils.database.Database;
 
 public class SQLServerDatabase extends Database {
-	public SQLServerDatabase(String environment, String host, String port, String dbName){
+	public SQLServerDatabase(String host, String port, String dbName){
 		setDbDriver("org.gjt.mm.mysql.Driver");
 		setDbConnectionString("jdbc:mysql://" + host + ":" + port + "/"+ dbName);
 	}
