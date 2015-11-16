928e456804bfedc9e1611fcf7e30df0c7dca2d6f
Justin Phlegar <Justin.Phlegar@orasi.com>
2015-11-16 17:14:36 -0500

    Closes #26. Created additional methods for frame handling

--- src/main/java/com/orasi/utils/FrameHandler.java
+++ src/main/java/com/orasi/utils/FrameHandler.java
@@ -1,5 +1,7 @@
 package com.orasi.utils;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -10,4 +12,63 @@ public class FrameHandler {
 		WebDriverWait wait = new WebDriverWait(driver,TestEnvironment.getDefaultTestTimeout());
 		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(frame));		
 	}
+	
+	public static String getCurrentFrameName(WebDriver driver){
+	    if(driver instanceof OrasiDriver){
+		driver = ((OrasiDriver) driver).getDriver();
+	    }
+	    
+	    String frameName =  ((JavascriptExecutor) driver).executeScript("return self.name").toString();
+	    if (frameName.isEmpty()) frameName = null;
+	    return frameName;
+	}
+	
+	public static void moveToDefaultContext(WebDriver driver){
+	    driver.switchTo().defaultContent();
+	}
+	
+	public static void moveToParentFrame(WebDriver driver){
+	    driver.switchTo().parentFrame();
+	}
+	
+	public static void moveToSiblingFrame(WebDriver driver, String frameName){
+	    moveToParentFrame(driver);
+	    switchToFrame(driver, frameName);
+	}
+	
+	public static void moveToSiblingFrame(WebDriver driver, By byFrameLocator){
+	    moveToParentFrame(driver);
+	    switchToFrame(driver, byFrameLocator);
+	}
+	
+	public static void moveToChildFrame(WebDriver driver, String frameName){
+	    switchToFrame(driver, frameName);
+	}
+	
+	public static void moveToChildFrame(WebDriver driver, By byFrameLocator){
+	    switchToFrame(driver, byFrameLocator);
+	}
+	
+	public static void moveToChildFrame(WebDriver driver, String[] frameName){
+	    for (int x = 0 ; x < frameName.length ; x++) {
+		moveToChildFrame(driver, frameName[x]);
+	    }
+	}
+
+	public static void moveToChildFrame(WebDriver driver, By[] frameName){
+	    for (int x = 0 ; x < frameName.length ; x++) {
+		moveToChildFrame(driver, frameName[x]);
+	    }
+	}
+	
+	private static void switchToFrame(WebDriver driver, String frameName){
+	    WebDriverWait wait = new WebDriverWait(driver, TestEnvironment.getDefaultTestTimeout());
+	    wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(frameName));	
+	}
+	
+	private static void switchToFrame(WebDriver driver, By byFrameLocator){
+	    WebDriverWait wait = new WebDriverWait(driver, TestEnvironment.getDefaultTestTimeout());
+	    wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(byFrameLocator));	
+	}
+   
 }
