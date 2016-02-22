2b87f427d757ca26d9b7770c42b8affa263e815e
Justin Phlegar <Justin.Phlegar@orasi.com>
2016-02-22 08:20:31 -0500

    Fixing element being located that would cause script to fail

--- src/test/java/com/orasi/core/interfaces/TestLink.java
+++ src/test/java/com/orasi/core/interfaces/TestLink.java
@@ -20,39 +20,39 @@ import ru.yandex.qatools.allure.annotations.Title;
 
 public class TestLink extends TestEnvironment {
     
-    @BeforeTest(groups ={"regression", "interfaces", "link", "dev"})
-    @Parameters({ "runLocation", "browserUnderTest", "browserVersion",
-	    "operatingSystem", "environment" })
-    public void setup(@Optional String runLocation, String browserUnderTest,
-	    String browserVersion, String operatingSystem, String environment) {
-	setApplicationUnderTest("Test Site");
-	setBrowserUnderTest(browserUnderTest);
-	setBrowserVersion(browserVersion);
-	setOperatingSystem(operatingSystem);
-	setRunLocation(runLocation);
-	setTestEnvironment(environment);
-	setPageURL("http://orasi.github.io/Selenium-Java-Core/sites/unitTests/orasi/core/interfaces/link.html");
-	testStart("TestLink");
-    }
+        @BeforeTest(groups ={"regression", "interfaces", "link", "dev"})
+        @Parameters({ "runLocation", "browserUnderTest", "browserVersion",
+    	    "operatingSystem", "environment" })
+        public void setup(@Optional String runLocation, String browserUnderTest,
+    	    String browserVersion, String operatingSystem, String environment) {
+        	setApplicationUnderTest("Test Site");
+        	setBrowserUnderTest(browserUnderTest);
+        	setBrowserVersion(browserVersion);
+        	setOperatingSystem(operatingSystem);
+        	setRunLocation(runLocation);
+        	setTestEnvironment(environment);
+        	setPageURL("http://orasi.github.io/Selenium-Java-Core/sites/unitTests/orasi/core/interfaces/link.html");
+        	testStart("TestLink");
+        }
     
-    @AfterTest(groups ={"regression", "interfaces", "link", "dev"})
-    public void close(ITestContext testResults){
-	endTest("TestAlert", testResults);
-    }
+        @AfterTest(groups ={"regression", "interfaces", "link", "dev"})
+        public void close(ITestContext testResults){
+    		endTest("TestAlert", testResults);
+        }
 
 	@Features("Element Interfaces")
 	@Stories("Link")
 	@Title("constructor")
 	@Test(groups ={"regression", "interfaces", "link"})
 	public void constructorWithElement(){
-		Assert.assertNotNull((new LinkImpl(getDriver().findLink(By.xpath("//a[@href='link.html']")))));
+		Assert.assertNotNull((new LinkImpl(getDriver().findLink(By.xpath("//a[@href='testLinks.html']")))));
 	}
 
 	@Features("Element Interfaces")
 	@Stories("Link")
 	@Title("click")
-    @Test(groups ={"regression", "interfaces", "link"})
-    public void click(){
+	@Test(groups ={"regression", "interfaces", "link"}, dependsOnMethods="constructorWithElement")
+	public void click(){
 		Link link= getDriver().findLink(By.xpath("//a[@href='testLinks.html']"));
 		link.click();
 		Assert.assertTrue(PageLoaded.syncVisible(getDriver(), getDriver().findElement(By.xpath("//a[@href='link.html']"))));
@@ -79,12 +79,12 @@ public class TestLink extends TestEnvironment {
 	@Features("Element Interfaces")
 	@Stories("Link")
 	@Title("jsClick")
-    @Test(groups ={"regression", "interfaces", "link"}, dependsOnMethods="click")
-    public void jsClick(){
+        @Test(groups ={"regression", "interfaces", "link"}, dependsOnMethods="click")
+        public void jsClick(){
 		Link link= getDriver().findLink(By.xpath("//a[@href='link.html']"));
 		link.jsClick();
 		Assert.assertTrue(PageLoaded.syncVisible(getDriver(), getDriver().findLink(By.xpath("//a[@href='testLinks.html']"))));
-    }
+	}
 
 	@Features("Element Interfaces")
 	@Stories("Link")
@@ -106,9 +106,9 @@ public class TestLink extends TestEnvironment {
 	@Features("Element Interfaces")
 	@Stories("Link")
 	@Title("getURL")
-    @Test(groups ={"regression", "interfaces", "link"}, dependsOnMethods="jsClick")
-    public void getURL(){
-	Link link= getDriver().findLink(By.xpath("//a[@href='testLinks.html']"));
-	Assert.assertTrue(link.getURL().contains("testLinks.html"));
-    }
+        @Test(groups ={"regression", "interfaces", "link"}, dependsOnMethods="jsClick")
+        public void getURL(){
+        	Link link= getDriver().findLink(By.xpath("//a[@href='testLinks.html']"));
+        	Assert.assertTrue(link.getURL().contains("testLinks.html"));
+        }
 }
