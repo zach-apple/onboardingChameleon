3dde6093fa50d9be883c981caec65be1a5850e6a
jessicamarshall <jessica.marshall@orasi.com>
2016-09-30 13:36:13 -0400

    had to add a few pieces of code to get the gecko driver to work

--- src/main/java/com/orasi/utils/OrasiDriver.java
+++ src/main/java/com/orasi/utils/OrasiDriver.java
@@ -758,12 +758,15 @@ public class OrasiDriver implements WebDriver, JavaScriptExecutor, TakesScreensh
 	private void setDriverWithCapabilties(DesiredCapabilities caps) {
 		switch (caps.getBrowserName().toLowerCase()) {
 		case "firefox":
-			driver = new FirefoxDriver(caps);
-			break;
-		case "marionette":
-			driver = new MarionetteDriver(caps);
+			if (caps.getCapability("marionette").equals(true)){
+				driver = new MarionetteDriver(caps);
+			} else {
+				driver = new FirefoxDriver(caps);
+			}
+			
 			break;
 		case "internet explorer":
+		case "iexplore":
 		case "ie":
 			driver = new InternetExplorerDriver(caps);
 			//driver = new SynchronizedInternetExplorerDriver(caps);			
