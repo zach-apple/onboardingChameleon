cf3bf3bd8c1b555175f3c03aeebf6e03ec326f93
JustinPhlegar <Phlegar85@yahoo.com>
2017-03-12 17:39:04 -0400

    Removing JXL depenancies. Allow xls, xlsx and csv data files for SoapService data input.

--- src/main/java/com/orasi/utils/dataProviders/CSVDataProvider.java
+++ src/main/java/com/orasi/utils/dataProviders/CSVDataProvider.java
@@ -100,4 +100,86 @@ public class CSVDataProvider {
 		delimiter = delimiterValue;
 		return getTestScenarioData(filePath);
 	}
+	
+	public static Object[][] getData(String filePath){
+		BufferedReader bufferedReader = null;
+		FileReader fileReader = null;
+		String line = "";
+		String csvSplitBy = ",";
+		String[][] dataArray = null;
+		List<String>csvRowList = new ArrayList<String>();
+		String[] rowSplit;
+		int columnCount = 0;
+		int rowCount = 0;
+		
+		// Get the file location from the project main/resources folder
+		if(!filePath.contains(":")) filePath =  CSVDataProvider.class.getResource(filePath).getPath();
+
+		// in case file path has a %20 for a whitespace, replace with actual
+		// whitespace
+		filePath = filePath.replace("%20", " ");
+		
+		TestReporter.logTrace("File path of CSV to open [ " + filePath + " ]");
+		//open the CSV and add each line into a list		
+		try {
+			TestReporter.logTrace("Loading file into FileReader");
+			fileReader = new FileReader(filePath);
+			TestReporter.logTrace("Successfully loaded file into FileReader");
+			
+			TestReporter.logTrace("Loading FileReader object into BufferedReader");
+			bufferedReader = new BufferedReader(fileReader);
+			TestReporter.logTrace("Successfully loaded FileReader object into BufferedReader");
+			
+			TestReporter.logTrace("Read in file and load each line into a List");
+			while((line = bufferedReader.readLine()) !=null) {
+				csvRowList.add(line);
+			}
+			TestReporter.logTrace("Successfully read in [ " + csvRowList.size() + " ] lines from file");
+		} catch (IOException e) {
+			throw new AutomationException("Failed to read in CSV file", e);
+		}finally{
+			try {
+				TestReporter.logTrace("Closing BufferedReader");
+				bufferedReader.close();
+				TestReporter.logTrace("Successfully closed BufferedReader");
+
+				TestReporter.logTrace("Closing FileReader");
+				fileReader.close();
+				TestReporter.logTrace("Successfully closed FileReader");
+			} catch (IOException throwAway) {}
+		}
+		
+		TestReporter.logTrace("Determining column count based on delimiter [ " + delimiter + " ]");
+		columnCount = csvRowList.get(0).split(delimiter).length;
+		TestReporter.logTrace("Found [ " + columnCount + " ] columns");
+
+		TestReporter.logTrace("Determining row count");
+		rowCount = csvRowList.size();
+		TestReporter.logTrace("Found [ " + rowCount + " ] rows");
+		
+		TestReporter.logTrace("Attempting to create an array based on rows and columns. Array to built [" + rowCount + "][" + columnCount + "]");
+		dataArray = new String[rowCount][columnCount]; 
+		
+		//transform the list into 2d array
+		//start at row 1 since, first row 0 is column headings
+		TestReporter.logTrace("Transferring data to Array");
+		for (int rowNum = 0; rowNum <= rowCount-1; rowNum++) {
+			
+			//take the row which is a string, and split it
+			rowSplit = csvRowList.get(rowNum).split(delimiter);
+			
+			for (int colNum = 0; colNum < columnCount; colNum++ ) {
+				dataArray[rowNum][colNum] = rowSplit[colNum];			
+			}
+		}
+		
+		TestReporter.logTrace("Exiting CSVDataProvider#getTestScenarioData");
+		return dataArray;	
+	}
+
+	public static Object[][] getData(String filePath, String delimiterValue){
+		TestReporter.logTrace("Overriding default delimiter of [ , ] to be [ " + delimiter + " ]");
+		delimiter = delimiterValue;
+		return getData(filePath);
+	}
 }
