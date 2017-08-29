91f6366dc4145c1df3f058c0f7d11390c076297d
Justin Phlegar <phlegarj85@yahoo.com>
2017-08-28 23:02:06 -0400

    Condensing code

--- src/main/java/com/orasi/utils/JavaUtilities.java
+++ src/main/java/com/orasi/utils/JavaUtilities.java
@@ -1,43 +1,24 @@
 package com.orasi.utils;
 
+import static org.apache.commons.lang3.StringUtils.isEmpty;
+
 import java.util.Collection;
 import java.util.Map;
 
-import org.apache.commons.lang3.StringUtils;
-
 public class JavaUtilities {
 
-	public static boolean isValid(Object obj){
-		boolean isValid = false;
-		
-		if(obj == null){
-			return isValid;
-		}else{
-			isValid = true;
-		}
-		
-		if(isValid){
-			if(obj instanceof String){
-				if(StringUtils.isEmpty(((String) obj))){
-					return false;
-				}else{
-					return true;
-				}
-			}else if(obj instanceof Collection<?>){
-				if(((Collection<?>) obj).isEmpty()){
-					return false;
-				}else{
-					return true;
-				}
-			}else if(obj instanceof Map<?, ?>){
-				if(((Map<?,?>) obj).isEmpty()){
-					return false;
-				}else{
-					return true;
-				}
-			}
-		}
-		
-		return isValid;
-	}
+    public static boolean isValid(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (obj instanceof String && isEmpty((String) obj)) {
+            return false;
+        } else if (obj instanceof Collection<?> && ((Collection<?>) obj).isEmpty()) {
+            return false;
+        } else if (obj instanceof Map<?, ?> && ((Map<?, ?>) obj).isEmpty()) {
+            return false;
+        }
+        return true;
+    }
 }
