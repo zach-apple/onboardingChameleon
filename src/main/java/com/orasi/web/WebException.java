d85a73950e3a7fa6ecffd0c10fdeef5137706312
JustinPhlegar <Phlegar85@yahoo.com>
2017-08-31 00:00:11 -0400

    Removing unused code

--- src/main/java/com/orasi/web/WebException.java
+++ src/main/java/com/orasi/web/WebException.java
@@ -3,11 +3,7 @@ package com.orasi.web;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.InetAddress;
 import java.net.URL;
-import java.net.UnknownHostException;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
@@ -30,7 +26,6 @@ public class WebException extends AutomationException {
     public static final String DRIVER_INFO = "Driver info";
     public WebDriver driver = null;
 
-    private Map<String, String> extraInfo = new HashMap<>();
     private static final long serialVersionUID = -8710980695994382082L;
 
     public WebException(String message) {
@@ -66,43 +61,12 @@ public class WebException extends AutomationException {
                 + getGridInfo(driver);
     }
 
-    public String getSystemInformation() {
-        String host = "N/A";
-        String ip = "N/A";
-
-        try {
-            host = InetAddress.getLocalHost().getHostName();
-            ip = InetAddress.getLocalHost().getHostAddress();
-        } catch (UnknownHostException throw_away) {
-        }
-
-        return String.format("System info: host: '%s', ip: '%s', os.name: '%s', os.arch: '%s', os.version: '%s', java.version: '%s'",
-                host,
-                ip,
-                System.getProperty("os.name"),
-                System.getProperty("os.arch"),
-                System.getProperty("os.version"),
-                System.getProperty("java.version"));
-    }
-
     public String getSupportUrl() {
         return null;
     }
 
     public BuildInfo getBuildInformation() {
         return new BuildInfo();
-    }
-
-    public static String getDriverName(StackTraceElement[] stackTraceElements) {
-        String driverName = "unknown";
-        for (StackTraceElement e : stackTraceElements) {
-            if (e.getClassName().endsWith("Driver")) {
-                String[] bits = e.getClassName().split("\\.");
-                driverName = bits[bits.length - 1];
-            }
-        }
-
-        return driverName;
     }
 
     public String getDriverInfo(WebDriver driver) {
@@ -128,26 +92,6 @@ public class WebException extends AutomationException {
                 osInfo,
                 sessionId).trim();
         return info;
-    }
-
-    public void addInfo(String key, String value) {
-        extraInfo.put(key, value);
-    }
-
-    public String getAdditionalInformation() {
-        if (!extraInfo.containsKey(DRIVER_INFO)) {
-            extraInfo.put(DRIVER_INFO, "driver.version: " + getDriverName(getStackTrace()));
-        }
-
-        String result = "";
-        for (Map.Entry<String, String> entry : extraInfo.entrySet()) {
-            if (entry.getValue() != null && entry.getValue().startsWith(entry.getKey())) {
-                result += "\n" + entry.getValue();
-            } else {
-                result += "\n" + entry.getKey() + ": " + entry.getValue();
-            }
-        }
-        return result;
     }
 
     private String browserName(WebDriver driver) {
