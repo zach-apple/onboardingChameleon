3369fc26119ca5b807bd8ca2d1c1a7c15d547bc2
Justin Phlegar <Justin.Phlegar@orasi.com>
2017-09-18 09:56:33 -0400

    Readding TNS setting to OracleDatabase

--- src/main/java/com/orasi/database/databaseImpl/OracleDatabase.java
+++ src/main/java/com/orasi/database/databaseImpl/OracleDatabase.java
@@ -6,6 +6,12 @@ import com.orasi.utils.Constants;
 public class OracleDatabase extends Database {
 
     public OracleDatabase(String tnsName) {
+        String tns = getClass()
+                .getResource(Constants.TNSNAMES_PATH + "tnsnames.ora")
+                .getPath().toString();
+        tns = tns.substring(0, tns.lastIndexOf("/"));
+        System.setProperty("oracle.net.tns_admin", tns);
+
         super.driver = "oracle.jdbc.driver.OracleDriver";
         super.connectionString = "jdbc:oracle:thin:@" + tnsName.toUpperCase();
     }
